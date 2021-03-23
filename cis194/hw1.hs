2e9611f2ebaa6995b22ea395261a6472a9cee17c
taylnath <taylnath@oregonstate.edu>
2021-03-22 21:53:44 -0700

    x

--- cis194/hw1.hs
+++ cis194/hw1.hs
@@ -13,8 +13,8 @@ toDigits n = reverse $ toDigitsRev n
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther [] = []
 doubleEveryOther [n] = [n]
-doubleEveryOther ns = 
-    doubleEveryOther (init $ init ns) ++ [2 * (last $ init ns)] ++ [last ns]
+doubleEveryOther xs = 
+    doubleEveryOther (init $ init xs) ++ [2 * (last $ init xs)] ++ [last xs]
 
 -- convert a list of numbers to a list of single-digit numbers
 -- by writing each number > 10 as its digits
@@ -22,14 +22,46 @@ singleDigits :: [Integer] -> [Integer]
 singleDigits [] = []
 singleDigits (x:xs) = (toDigits x) ++ (singleDigits xs)
 
+-- sums the digits of all numbers in the list
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits [n] = n
+sumDigits (x:xs) = x + (sumDigits xs)
+
+-- check if the given numbers form a valid credit card according to the rules
+validate :: Integer -> Bool
+validate n
+    | n <= 0    = False
+    | otherwise = (mod (sumDigits $ singleDigits $ doubleEveryOther $ toDigits n) 10) == 0
+
 testNum n = do 
     print n
     print $ toDigits n
     print $ toDigitsRev n
     print $ doubleEveryOther $ toDigits n
     print $ singleDigits $ doubleEveryOther $ toDigits n
+    print $ sumDigits $ singleDigits $ doubleEveryOther $ toDigits n
+    print $ validate n
+
+testValidate = do
+    testNum 8765
+    testNum 4012888888881881
+    testNum 4012888888881882
+
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 a b c = []
+hanoi 1 a b c = [(a, b)]
+hanoi n a b c = (hanoi (n-1) a c b) ++ [(a,b)] ++ (hanoi (n-1) c b a)
+
+testHanoi n = do
+    print n
+    print $ hanoi n "a" "b" "c"
 
 main = do
-    testNum 8765
-    testNum 0
-    testNum 123
+    testHanoi 0
+    testHanoi 1
+    testHanoi 2
+    testHanoi 3
+    testHanoi 4

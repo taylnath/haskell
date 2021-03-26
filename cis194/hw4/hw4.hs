984d494fd6c8a6bffcbb04ed94d1b821c07e0c0b
taylnath <taylnath@oregonstate.edu>
2021-03-26 01:20:52 -0700

    x

--- cis194/hw4/hw4.hs
+++ cis194/hw4/hw4.hs
@@ -15,6 +15,31 @@ fun2 n
     | even n    = n + fun2 (n `div` 2)
     | otherwise = fun2 (3*n + 1)
 
+fun2' :: Integer -> Integer
+fun2' 0 = 0
+fun2' n = 
+    let s = span even (iterate (\x -> div x 2) n) 
+    in sum (fst s) -- + fun2' (head $ snd s)
+
+data Tree a = Leaf 
+    | Node Integer (Tree a) a (Tree a)
+    deriving (Show, Eq)
+
+-- generates a balanced (height of left/right subtrees recursively 
+-- differ by at most 1) binary tree from the list of values, using foldr.
+insert :: a -> Tree a -> Tree a
+insert x Leaf = Node 0 Leaf x Leaf
+insert x (Node h Leaf y Leaf) = Node (h+1) (Node 0 Leaf x Leaf) y Leaf
+insert x (Node h Leaf y r) = Node h (Node 0 Leaf x Leaf) y r
+insert x (Node h l y Leaf) = Node h l y (Node 0 Leaf x Leaf)
+insert x (Node h (Node hL lL yL rL) y (Node hR lR yR rR))
+    | hL < hR   = Node h (insert x (Node hL lL yL rL)) y (Node hR lR yR rR)
+    | otherwise = Node h (Node hL lL yL rL) y (insert x (Node hR lR yR rR))
+
+
+foldTree :: [a] -> Tree a
+foldTree xs = foldr (\x y -> Leaf) Leaf xs
+
 main = do
     print $ and $ map (\x -> fun1 x == fun1' x) 
         [
@@ -25,3 +50,4 @@ main = do
             [1,1,4,5,6,7,8],
             [-10,38,5,9,11]
         ]
+    mapM (\x -> print (fun2 x == fun2' x)) [-10,1,2,3,0,4,5,6]

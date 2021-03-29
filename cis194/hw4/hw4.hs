5f6ceb59c9c92b567bac4a694bc9c88361f59ad7
taylnath <taylnath@oregonstate.edu>
2021-03-28 21:25:56 -0700

    done with ex1 sort of

--- cis194/hw4/hw4.hs
+++ cis194/hw4/hw4.hs
@@ -1,5 +1,8 @@
+---------------------------------------------------------
 -- Ex 1: re-implement fun1 and fun2 more idiomatically, 
 -- potentially using iterate and takeWhile
+---------------------------------------------------------
+
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
 fun1 (x:xs)
@@ -16,16 +19,39 @@ fun2 n
     | even n    = n + fun2 (n `div` 2)
     | otherwise = fun2 (3*n + 1)
 
-fun2' :: Integer -> Integer
-fun2' 0 = 0
-fun2' n = 
-    let s = span even (iterate (\x -> div x 2) n) 
-    in sum (fst s) -- + fun2' (head $ snd s)
+eo1 :: Integer -> Bool
+eo1 x = ((even x) || (x == 1)) && (x /= 0)
 
+init' :: [Integer] -> [Integer]
+init' [] = []
+init' [x] = [x]
+init' x = init x
+
+nList :: Integer -> [Integer]
+nList 0 = []
+nList 1 = []
+nList x = 
+    let s = span eo1 (iterate (\y -> div y 2) x)
+    in (init'.fst) s ++ nList ((head $ snd s) * 3 + 1)
+
+fun2' :: Integer -> Integer
+fun2' n = (sum.nList) n
+
+---------------------------------------------------------
+-- Ex 2: See foldTree.hs
+---------------------------------------------------------
+
+---------------------------------------------------------
+-- Ex 3 Part 1:
+---------------------------------------------------------
 
 -- xor returns True iff number of True values is odd.
 xor :: [Bool] -> Bool
 xor xs = foldr (\x y -> (x || y) && not (x && y)) False xs
+
+---------------------------------------------------------
+-- Ex 3 Part 2:
+---------------------------------------------------------
 
 -- map implemented as a fold
 map' :: (a -> b) -> [a] -> [b]
@@ -41,7 +67,8 @@ main = do
             [1,1,4,5,6,7,8],
             [-10,38,5,9,11]
         ]
-    -- mapM (\x -> print (fun2 x == fun2' x)) [-10,1,2,3,0,4,5,6]
+    print $ and $ map (\x -> fun2 x == fun2' x) [10,1,2,3,4,5,6]
+    -- mapM (\x -> print (fun2 x == fun2' x)) [10,1,2,3,4,5,6]
     putStr "\nTesting xor:\n"
     mapM (\x -> print (xor x)) 
         [

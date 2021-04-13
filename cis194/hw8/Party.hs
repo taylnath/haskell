e4b07f778ba0a3b637e5fc1d289035b2b4716540
Nathan Taylor <taylnath@oregonstate.edu>
2021-04-13 15:57:10 -0700

    finished hw8 ex 4 -- without old comments

--- cis194/hw8/Party.hs
+++ cis194/hw8/Party.hs
@@ -34,6 +34,7 @@ moreFun g1@(GL e1 f1) g2@(GL e2 f2)
 -- Ex 2
 -------------------------------------------------------------
 
+-- From Data.Tree:
 -- data Tree a = Node {
 --   rootLabel :: a, -- label value
 --   subForest :: [Tree a] -- zero or more child trees
@@ -43,19 +44,6 @@ moreFun g1@(GL e1 f1) g2@(GL e2 f2)
 treeFold :: b -> (a -> [b] -> b) -> Tree a -> b
 treeFold e f (Node a []) = f a [e]
 treeFold e f (Node a bs) = f a (map (treeFold e f) bs)
--- treeFold :: (b -> a -> b) -> b -> Tree a -> b
--- treeFold f b (Node a []) = f b a
--- treeFold f b (Node a (t:ts)) = treeFold f (treeFold f t )
--- treeFold f b (Node a (t:ts)) = tF (tF f t )
---   where tF st tr = treeFold f st tr
-
--- treeFold f b (Node a [Node a1 []]) = treeFold (f b a) (Node a1 []))
---                       = f (f b a) a1
---                       = (b `f` a) `f` a1
-
--- treeFold f b (Node a [Node a1 [], Node a2 []])
---   = ((b `f` a) `f` a1) `f` a2
---   = foldl 
 
 -------------------------------------------------------------
 -- Ex 3
@@ -76,30 +64,20 @@ collapseGL glPairs = mconcat $ map maxSndFlip glPairs
 
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
 -- if the employee is no-ones boss, they should go
--- (take this line out?)
 nextLevel boss [] = (GL [boss] (empFun boss), GL [] 0)
 -- if the boss goes, the sub-bosses will have no fun, so boss gets added to glNoBoss
 -- if the boss doesn't go, take the maximum between the sub-bosses going or not
 nextLevel boss glPairs = (boss `glCons` glNoBoss, glBoss)
   where (glNoBoss, glBoss) = collapseGL glPairs
--- nextLevel boss [(glBoss, glNoBoss)] = (boss `glCons` glNoBoss, moreFun glBoss glNoBoss)
--- nextLevel boss glPairs = (boss `glCons` glNoBoss, )
---   where (glBoss, glNoBoss) = mconcat $ map ()
-
--- nextLevel boss [(gl1, gl2)]
-  -- | 
--- data GuestList = GL [Employee] Fun
 
 -------------------------------------------------------------
 -- Ex 4
 -------------------------------------------------------------
 
+-- get the max of the pair
 maxPair :: (Ord a) => (a,a) -> a
 maxPair (x,y) = max x y
 
 -- outputs a fun-maximizing guest list
 maxFun :: Tree Employee -> GuestList
 maxFun t = maxPair $ treeFold (GL [] 0, GL [] 0) nextLevel t
--- maxFun (Node e []) = GL [e] (empFun e)
--- maxFun (Node e es) = maxFun 
--- maxFun (Node e [(Node e1 []), (Node e2 [])]) = max ()
